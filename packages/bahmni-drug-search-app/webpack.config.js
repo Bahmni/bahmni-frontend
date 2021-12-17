96a1b9ed3167caf47f73695b55c46d727c2f2968
MOHANKUMAR T <mohan13081999@gmail.com>
2021-12-17 12:04:59 +0530

    BAH-1306|Mohan/Sanofer|Refactor| Update webpack output directory

--- packages/bahmni-drug-search-app/webpack.config.js
+++ packages/bahmni-drug-search-app/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './index.tsx',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, '../../dist/drug-search-app'),
     filename: 'main.js',
   },
   devServer: {

ee7762beee4629ec1526fa8bdd9a4567481195cc
_n0man <nouman.memon@icloud.com>
2021-12-22 11:18:01 +0530

    _n0man | manage importMap in localStorage

--- packages/bahmni-mf-app-shell/src/importmap.js
+++ packages/bahmni-mf-app-shell/src/importmap.js
@@ -1,3 +1,10 @@
-const importMap = {
-  medication: [{ name: 'medication-app', enabled: true, urls: ['/bahmni/mf/bahmni-medication/bundle.js'] }],
-};
+if (!window.localStorage.hasOwnProperty('importMap')) {
+  window.localStorage.setItem(
+    'importMap',
+    JSON.stringify({
+      medication: [{ name: 'medication-app', enabled: true, urls: ['/bahmni/mf/bahmni-medication/bundle.js'] }],
+    }),
+  );
+}
+
+const importMap = JSON.parse(window.localStorage.getItem('importMap'));

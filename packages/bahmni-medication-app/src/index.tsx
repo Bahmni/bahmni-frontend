543413a6fff13c15baa9012f43ed5ad4e4aa347e
MOHANKUMAR T <mohan13081999@gmail.com>
2021-12-27 10:16:41 +0530

    BAH-1312 | refactor. medication app styling
    Co-authored-by: Arjun G <arjun.g@thoughtworks.com>

--- packages/bahmni-medication-app/src/index.tsx
+++ packages/bahmni-medication-app/src/index.tsx
@@ -6,8 +6,10 @@ import PrescriptionDialog from './components/PrescriptionDialog';
 
 const styles = {
   container: {
-    width: '70%',
     margin: '1rem 0 0 1rem',
+  },
+  search_bar: {
+    width: '70%',
   },
   tileList: {
     margin: 'auto',
@@ -60,16 +62,18 @@ const MedicationApp = () => {
   const handlePrescriptionDialogClose = () => {};
   return (
     <div style={styles.container}>
-      <Search
-        id="search"
-        data-testid="Search Drug"
-        labelText="SearchDrugs"
-        placeholder="Search for drug to add in prescription"
-        onChange={(e) => handleUserInput(e)}
-        onClear={() => clearUserInput()}
-        value={userInput}
-      />
-      <div style={styles.tileList}>{showDrugOptions()}</div>
+      <div style={styles.search_bar}>
+        <Search
+          id="search"
+          data-testid="Search Drug"
+          labelText="SearchDrugs"
+          placeholder="Search for drug to add in prescription"
+          onChange={(e) => handleUserInput(e)}
+          onClear={() => clearUserInput()}
+          value={userInput}
+        />
+        <div style={styles.tileList}>{showDrugOptions()}</div>
+      </div>
       {selectedDrug && (
         <PrescriptionDialog drug={selectedDrug} onClose={handlePrescriptionDialogClose}></PrescriptionDialog>
       )}

4a77b7b06e8d6de72479c8ccfe2a53de6dbccaaa
SanoferSameera <sanofersameera25@gmail.com>
2021-12-29 19:20:17 +0530

    Added. test for active prescription table
    Co-authored-by: Abinaya <abinaya.u@thoughtworks.com>

--- packages/bahmni-medication-app/src/__tests__/mockHelper.ts
+++ packages/bahmni-medication-app/src/__tests__/mockHelper.ts
@@ -15,3 +15,57 @@ export const mockDrugsApiResponse = {
     results: [],
   },
 };
+
+export const mockActivePrescriptionResponse = [
+  {
+    visit: {
+      startDateTime: 1496851128000,
+    },
+    dateStopped: null,
+    provider: {
+      name: 'Super Man',
+    },
+    drug: {
+      name: 'Aspirin 75mg',
+      form: 'Tablet',
+    },
+    dosingInstructions: {
+      dose: 5.0,
+      doseUnits: 'Capsule(s)',
+      route: 'Oral',
+      frequency: 'Thrice a day',
+      administrationInstructions: '{"instructions":"As directed"}',
+      quantity: 150.0,
+      quantityUnits: 'Capsule(s)',
+    },
+    durationUnits: 'Day(s)',
+    effectiveStartDate: 1640164841000,
+  },
+];
+
+export const mockPrescriptionResponse = [
+  {
+    visit: {
+      startDateTime: 1496851128000,
+    },
+    dateStopped: '1607888143',
+    provider: {
+      name: 'Super Man',
+    },
+    drug: {
+      name: 'Aspirin 75mg',
+      form: 'Tablet',
+    },
+    dosingInstructions: {
+      dose: 5.0,
+      doseUnits: 'Capsule(s)',
+      route: 'Oral',
+      frequency: 'Thrice a day',
+      administrationInstructions: '{"instructions":"As directed"}',
+      quantity: 150.0,
+      quantityUnits: 'Capsule(s)',
+    },
+    durationUnits: 'Day(s)',
+    effectiveStartDate: 1640164841000,
+  },
+];

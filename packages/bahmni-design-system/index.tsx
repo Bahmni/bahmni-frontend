8d1dea78e815d4190f07ea9d89fc81af66cd2b3e
_n0man <nouman.memon@icloud.com>
2022-01-05 17:05:25 +0530

    fixed linting issues with bahmni-design-system | replaced jsxBracketSameLine config

--- packages/bahmni-design-system/index.tsx
+++ packages/bahmni-design-system/index.tsx
@@ -1,9 +1,9 @@
-import Button from 'carbon-components-react/es/components/Button';
-import Search from 'carbon-components-react/es/components/Search';
-import { ClickableTile } from 'carbon-components-react/es/components/Tile';
-import Tab from 'carbon-components-react/es/components/Tab/Tab';
-import Tabs from 'carbon-components-react/es/components/Tabs';
-import Link from 'carbon-components-react/es/components/Link';
+import Button from 'carbon-components-react/es/components/Button'
+import Search from 'carbon-components-react/es/components/Search'
+import {ClickableTile} from 'carbon-components-react/es/components/Tile'
+import Tab from 'carbon-components-react/es/components/Tab/Tab'
+import Tabs from 'carbon-components-react/es/components/Tabs'
+import Link from 'carbon-components-react/es/components/Link'
 import {
   Table,
   TableBody,
@@ -11,13 +11,13 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from 'carbon-components-react/es/components/DataTable';
-import { Grid, Row, Column } from 'carbon-components-react/es/components/Grid';
-import NumberInput from 'carbon-components-react/es/components/NumberInput';
-import Dropdown from 'carbon-components-react/es/components/Dropdown';
-import DatePicker from 'carbon-components-react/es/components/DatePicker';
-import DatePickerInput from 'carbon-components-react/es/components/DatePickerInput';
-import Tag from 'carbon-components-react/es/components/Tag/Tag';
+} from 'carbon-components-react/es/components/DataTable'
+import {Grid, Row, Column} from 'carbon-components-react/es/components/Grid'
+import NumberInput from 'carbon-components-react/es/components/NumberInput'
+import Dropdown from 'carbon-components-react/es/components/Dropdown'
+import DatePicker from 'carbon-components-react/es/components/DatePicker'
+import DatePickerInput from 'carbon-components-react/es/components/DatePickerInput'
+import Tag from 'carbon-components-react/es/components/Tag/Tag'
 export {
   Button,
   Search,
@@ -39,4 +39,4 @@ export {
   Tabs,
   Link,
   Tag,
-};
+}

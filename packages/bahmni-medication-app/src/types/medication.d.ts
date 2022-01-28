a27abd5d3614f97561befe8f706828e5865d7e0b
Abinaya U <abinaya.u@thoughtworks.com>
2022-01-28 18:36:09 +0530

    BAH-1371 refactored. Prescription item type defenitions
    Co-authored-by: MOHANKUMAR T <mohan13081999@gmail.com>

--- packages/bahmni-medication-app/src/types/medication.d.ts
+++ packages/bahmni-medication-app/src/types/medication.d.ts
@@ -1,14 +1,63 @@
 export interface PrescriptionItem {
-  visit: any
-  dateStopped: number
+  action?: string
+  autoExpireDate?: number
+  careSetting: string
+  commentToFulfiller: string
+  concept?: Concept
+  creatorName?: string
   dateActivated: number
-  provider: any
-  concept?: any
-  drug: any
+  dateStopped: number
+  dosingInstructions: DosingInstructions
+  dosingInstructionType?: string
+  drug: DrugInfo
   drugNonCoded?: string
-  dosingInstructions: any
   duration: number
   durationUnits: string
   effectiveStartDate: number
   effectiveStopDate: number
+  instructions?: any
+  orderAttributes?: any
+  orderGroup?: any
+  orderNumber?: string
+  orderReasonConcept?: {uuid: string; name: string}
+  orderReasonText?: string
+  orderType?: string
+  previousOrderUuid?: string
+  provider?: any
+  retired?: boolean
+  scheduledDate?: number
+  sortWeight?: any
+  uuid?: string
+  visit?: any
+}
+
+export interface Concept {
+  conceptClass?: string
+  dataType?: string
+  hiNormal?: string
+  lowNormal?: string
+  mappings?: []
+  name?: string
+  set?: boolean
+  shortName?: string
+  uuid: string
+}
+
+export interface DosingInstructions {
+  administrationInstructions?: string
+  asNeeded?: boolean
+  dose: number
+  doseUnits: string
+  frequency: string
+  numberOfRefills?: any
+  quantity: number
+  quantityUnits: string
+  route: string
+}
+
+export interface DrugInfo {
+  form: String
+  name: String
+  strength: String
+  uuid: String
 }

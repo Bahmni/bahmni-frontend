44f8f56ff9e9e660c12760d0a63033d1c72bcf34
_n0man <nouman.memon@icloud.com>
2022-02-09 11:49:18 +0530

    fixed carbon imports

--- packages/omrs-design-system/src/components/floating-order-basket-button.component.tsx
+++ packages/omrs-design-system/src/components/floating-order-basket-button.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ShoppingBag16 from '@carbon/icons-react/es/shopping--bag/16'
-import {Button, Tag} from 'carbon-components-react'
+import Button from 'carbon-components-react/es/components/Button'
+import Tag from 'carbon-components-react/es/components/Tag'
 import {useTranslation} from 'react-i18next'
 import {connect} from 'unistore/react'
 import {

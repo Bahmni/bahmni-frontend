8b5e7959fe197b958603c2864037cefd2bda5e47
Priyanka <Priyanka_Ajmera@gap.com>
2022-03-29 11:25:17 +0530

    BAH-1493 | add. integrate lab lite patient details page with the new lab lite mf
    Co-authored by Aman Kumar Ayush <amankumar.ayush@thoughtworks.com>

--- packages/bahmni-lab-lite-app/src/PatientInfo/PatientInfo.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen, waitFor} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import MockAdapter from 'axios-mock-adapter/types'
-import {axe} from 'jest-axe'
-import {when} from 'jest-when'
-import React from 'react'
-import {REST_ENDPOINTS} from '../utils/constants'
-
-
